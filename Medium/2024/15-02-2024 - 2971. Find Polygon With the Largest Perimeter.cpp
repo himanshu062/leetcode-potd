77f9d30a4bd31ade6b27f7b95938d57e7fb7a4a4
Himanshu Garg <121446937+himanshu062@users.noreply.github.com>
2024-02-15 20:58:55 +0530

    Create 15-02-2024 - 2971. Find Polygon With the Largest Perimeter.cpp

---
+++ Medium/2024/15-02-2024 - 2971. Find Polygon With the Largest Perimeter.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long largestPerimeter(vector<int>& nums) {
+    long long prefix = accumulate(nums.begin(), nums.end(), 0LL);
+    ranges::sort(nums);
+    for (int i = nums.size() - 1; i >= 2; --i) {
+      prefix -= nums[i];
+      if (prefix > nums[i])
+        return prefix + nums[i];
+    }
+
+    return -1;    
+    }
+};
